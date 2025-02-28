00b380c96c71b3f4691540f8716d1b66620ae19a
Skyler Mayfield <mayfield@fynk.com>
2025-02-28 09:35:51 +0100

    initial commit

---
+++ vue-eglot-mode.el
@@ -0,0 +1,26 @@
+;;; vue-eglot-mode.el -*- lexical-binding: t -*-
+
+;; https://github.com/joaotavora/eglot/discussions/1184
+
+(require 'eglot)
+(require 'web-mode)
+
+;;;###autoload
+(define-derived-mode vue-eglot-mode web-mode "Vue")
+
+(defun vue-eglot-init-options ()
+  (let ((tsdk-path (expand-file-name
+                    "lib"
+                    (string-trim-right
+                     (shell-command-to-string
+                      "npm list --parseable typescript | head -n1")))))
+    `(:typescript (:tsdk ,tsdk-path)
+                  :vue (:hybridMode :json-false))))
+
+(put 'vue-eglot-mode 'eglot-language-id "vue")
+
+(add-to-list 'eglot-server-programs
+             `(vue-eglot-mode . ("vue-language-server" "--stdio" :initializationOptions ,(vue-eglot-init-options))))
+
+
+(provide 'vue-eglot-mode)
